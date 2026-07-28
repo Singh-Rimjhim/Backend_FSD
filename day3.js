6c7d3b20b440346d56f96b4087d855b77e718063
2025b01010193 <rimjhim.25b01010193@abes.ac.in>
2026-07-28 11:27:58 +0530

    day3

---
+++ day3.js
@@ -0,0 +1,102 @@
+//promises for asynch
+//js single thread
+// const promiseOne=new Promise((resolve,reject)=>{
+//     console.log("promise task 1")
+//    // resolve("Promises passed by using resolve");
+//     let msg=true;
+//     if(!msg==true){
+//         console.log("message using promises failed")
+//     }else{
+//         console.log("error......")
+//     }  
+
+//     setTimeout(()=>{
+//         console.log(resolve());
+//     },2000)
+// });
+// promiseOne.then((result)=>{
+//     console.log(result);
+// }).catch((error)=>{
+//     console.log(error);
+// })
+
+
+
+// const promiseOne = new Promise((resolve, reject) => {
+
+//     console.log("Promise task 1");
+
+//     let msg = true;
+
+//     setTimeout(() => {
+
+//         if (msg) {
+//             resolve("Promise completed successfully");
+//         } else {
+//             reject("Promise failed");
+//         }
+
+//     }, 2000);
+
+// });
+
+// promiseOne
+// .then((result) => {
+//     console.log(result);
+// })
+// .catch((error) => {
+//     console.log(error);
+// });
+
+
+// const promiseOne=new Promise((resolve,reject)=>{
+//     console.log("promise task 1")
+//    // resolve("Promises passed by using resolve");
+//     let msg=true;
+//     if(!msg==true){
+//         console.log("message using promises failed")
+//     }else{
+//         console.log("error......")
+//     }  
+
+//     // setTimeout(()=>{
+//     //     console.log(resolve());
+//     // },2000)
+// });
+// promiseOne.then((result)=>{
+//     console.log(result);
+// }).catch((error)=>{
+//     console.log(error);
+// })
+
+
+//async //await 
+// console.log("1");
+// async function test(){
+//     console.log("2");
+// await console.log("3"); //aage wle ko left krkek pehle fxn ke bahar run hoga fir ye neeche wle execute honge
+//     console.log("4");
+
+// }
+// t1=test();
+// console.log("5");
+//create promises that will print username and password using  and if username and password not found then it will call
+//reject and print ERROR.........
+new Promise((resolve, reject)=>{
+    setTimeout(function (){
+        console.log("this is promises ");
+        let err = true;
+
+        if(!err){
+            resolve("user:ABC, password: 1234");
+        }else{
+            reject("Error: Data failed");
+        }
+    }, 2000);
+})
+.then((data)=>{
+    console.log("Success:",data);
+})
+.catch((error)=>{
+    console.log(error);
+});

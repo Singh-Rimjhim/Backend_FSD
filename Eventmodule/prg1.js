61d3028e71a109d0678edbf29b4faf52666af3f3
2025b01010193 <rimjhim.25b01010193@abes.ac.in>
2026-08-18 09:24:36 +0530

    Day-05

--- Eventmodule/prg1.js
+++ Eventmodule/prg1.js
@@ -1,8 +1,23 @@
 //event
 //eventemitter- on(emit(event, param),callback) registering event and listening to event, on(), emit()-triggering event/ create event/fire event
-const EventEmitter=required('events');
-const event=new EventEmitter();
-event.emit("greet"),()=>{
-    console.log("this is event emitter");
-}
-event.emit("greet");
+// const EventEmitter=required('events');
+// const event=new EventEmitter();
+// event.emit("greet"),()=>{
+//     console.log("this is event emitter");
+// }
+// event.on("greet",()=>{
+//     console.log("this is event emitter");
+// });
+// event.emit("greet");
+
+class MyEvent extends EventEmitter{}
+const events = new EventEmitter();
+events.on("greet", (name)=>{
+    console.log(`Hello, ${name}! This is event emitter`);  //template literal ${variable}
+
+})
+events.on("exit", ()=>{
+    console.log("This is exit event");
+});
+events.emit("greet","Ranjana");
+events.emit("exit");

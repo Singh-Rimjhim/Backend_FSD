ac0e23e5a440c640d943756663662996ad6c2b94
2025b01010193 <rimjhim.25b01010193@abes.ac.in>
2026-09-08 11:27:32 +0530

    prg 2

---
+++ Eventmodule/prg2.js
@@ -0,0 +1,8 @@
+//simulate DOM-like events handling in node.js using events
+//dispatchEvent
+// const event=new EventEmitter=require('event');
+// const event=new EventEmitter();
+// EventEmitterAsyncResource();
+// emitter.on();
+// emitter.emit('click');
+// emitter.emit('mouseover');

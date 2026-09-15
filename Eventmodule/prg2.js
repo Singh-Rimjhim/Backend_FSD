04d0719bdfb7fd5847614b54fd1908847c668caa
2025b01010193 <rimjhim.25b01010193@abes.ac.in>
2026-09-16 00:32:28 +0530

    git_ignore

--- Eventmodule/prg2.js
+++
@@ -1,8 +0,0 @@
-//simulate DOM-like events handling in node.js using events
-//dispatchEvent
-// const event=new EventEmitter=require('event');
-// const event=new EventEmitter();
-// EventEmitterAsyncResource();
-// emitter.on();
-// emitter.emit('click');
-// emitter.emit('mouseover');
